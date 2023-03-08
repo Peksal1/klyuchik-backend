ea785dd7e95917c98e6b37a5b37c696c3915db21
Ilja Laurs <iljal@peplink.com>
2023-03-08 16:57:11 +0200

    add guild-members api call

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const GUILD_NAME = "Ключик в дурку";
 const SERVER_NAME = "Howling Fjord";
 const REGION = "eu"; // or "us" for US servers
-const API_URL = `https://raider.io/api/v1/guilds/profile?region=${REGION}&realm=${SERVER_NAME}&name=${GUILD_NAME}&fields=raid_progression`;
+const API_URL = `https://raider.io/api/v1/guilds/profile?region=${REGION}&realm=${SERVER_NAME}&name=${GUILD_NAME}`;
 
 // add this middleware to allow requests from any domain
 app.use(function (req, res, next) {
@@ -13,15 +13,24 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Endpoint to fetch guild members
+app.get("/guild-members", async (req, res) => {
+  try {
+    const fetch = await import("node-fetch");
+    const response = await fetch.default(API_URL + "&fields=members");
+    const data = await response.json();
+    res.send(data.members);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Failed to fetch guild members" });
+  }
+});
+
+// Endpoint to fetch guild raid progression
 app.get("/guild", async (req, res) => {
   try {
     const fetch = await import("node-fetch");
-    const response = await fetch.default(API_URL, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch.default(API_URL + "&fields=raid_progression");
     const data = await response.json();
     res.send(data);
   } catch (error) {
