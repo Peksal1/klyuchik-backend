315dd11023522c83b22319889cac79fac687bbae
Ilja Laurs <iljal@peplink.com>
2023-03-17 10:59:41 +0200

    debug

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ app.post(
     check("wow_nickname").notEmpty(),
   ],
   async (req, res) => {
+    console.log(req);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
